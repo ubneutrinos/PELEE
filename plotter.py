fix significance pot scaling

plotter.py
@@ -749,11 +749,13 @@ class Plotter:
             if kind == "event_category":
                 try:
                     self.significance = self._sigma_calc_matrix(
-                        lee_hist, n_tot-lee_hist, scale_factor=1e20/self.pot, cov=cov)
+                        lee_hist, n_tot-lee_hist, scale_factor=1.3e21/self.pot, cov=cov)
                     self.significance_likelihood = self._sigma_calc_likelihood(
                         lee_hist, n_tot-lee_hist, np.sqrt(err_mc + err_ext + err_nue + err_dirt + err_nc), scale_factor=1.3e21/self.pot)
                 except (np.linalg.LinAlgError, ValueError) as err:
                     print("Error calculating the significance", err)
+                    self.significance = -1
+                    self.significance_likelihood = -1
 
         ax1.bar(bincenters, n_tot, facecolor='none',
                 edgecolor='none', width=0, yerr=exp_err)