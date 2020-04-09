c60e9e396b0696f29ef696ab51ffedceaae7f0fc
nfoppiani <nicolo.foppiani@gmail.com>
2020-04-09 12:07:03 -0400

    Unblinding plotter

---
+++ localSettings.py
@@ -0,0 +1,8 @@
+main_path = "/home/nic/Dropbox/MicroBooNE/PELEE/"
+ntuple_path = "/home/nic/Desktop/MicroBooNE/bnb_nue_analysis/PELEE/0304/run1/nuepresel/"
+pickle_path = "/home/nic/Dropbox/MicroBooNE/PELEE/pickles/"
+plots_path = "/home/nic/Dropbox/MicroBooNE/_fig/PELEE/"
+
+APPEND = '_nuepresel'
+
+fold = 'searchingfornues'
