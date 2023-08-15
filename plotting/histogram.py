fix sideband calculation

plotting/histogram.py
@@ -3,7 +3,7 @@ import unittest
 import pandas as pd
 from uncertainties import correlated_values, unumpy
 from .category_definitions import get_category_label, get_category_color
-from .statistics import covariance, constrained_covariance, error_propagation_division
+from .statistics import covariance, sideband_constraint_correction, error_propagation_division
 
 
 class HistGenMixin:
@@ -303,18 +303,17 @@ class RunHistGenerator(HistGenMixin):
         use_sideband = use_sideband and self.sideband_generator is not None
         if include_multisim_errors:
             if use_sideband:
+                # We need the hist-generator to calculate the universe histograms later
                 sideband_hist_generator = self.sideband_generator.get_hist_generator(which="mc")
-                sideband_central_value_hist = sideband_hist_generator.generate()
-                sideband_data_hist_generator = self.sideband_generator.get_hist_generator(which="data")
-                sideband_data_hist = sideband_data_hist_generator.generate()
+                # We calculate the nominal values and *full* covariance matrix for the sideband data
+                sideband_mc_cv_hist = self.sideband_generator.get_mc_hist(include_multisim_errors=True)
                 # The extended covariance matrix contains the covariance of the concatenated
                 # central value and sideband data histograms.
                 extended_cov_mat = np.zeros(
-                    (hist.n_bins + sideband_data_hist.n_bins, hist.n_bins + sideband_data_hist.n_bins)
+                    (hist.n_bins + sideband_mc_cv_hist.n_bins, hist.n_bins + sideband_mc_cv_hist.n_bins)
                 )
-                extended_central_value = np.concatenate((hist.nominal_values, sideband_data_hist.nominal_values))
-            else:
-                sideband_hist_generator = None
+                extended_central_value = np.concatenate((hist.nominal_values, sideband_mc_cv_hist.nominal_values))
+                sideband_cov_mat = np.zeros((sideband_mc_cv_hist.n_bins, sideband_mc_cv_hist.n_bins))
             for ms_column in ["weightsGenie", "weightsFlux", "weightsReint"]:
                 # The GENIE variations are applied instead of the central value tuning, so we need to use the
                 # weights_no_tune column instead of the weights column.
@@ -326,40 +325,51 @@ class RunHistGenerator(HistGenMixin):
                     extra_query=extra_query,
                     return_histograms=True,
                 )
+                hist.add_covariance(cov_mat)
                 if not use_sideband:
-                    hist.add_covariance(cov_mat)
                     continue
 
-                _, sideband_universe_hists = sideband_hist_generator.calculate_multisim_uncertainties(
+                sb_cov_mat, sideband_universe_hists = sideband_hist_generator.calculate_multisim_uncertainties(
                     ms_column,
                     weight_column=weight_column,
-                    central_value_hist=sideband_central_value_hist,
+                    # when calculating multisim uncertainties, the central value is that of MC alone
+                    central_value_hist=sideband_mc_cv_hist,
                     return_histograms=True,
                 )
                 concat_observations = np.concatenate([universe_hists, sideband_universe_hists], axis=1)
-                extended_cov_mat += covariance(concat_observations, extended_central_value)
-            
-            # we have to run the unisim calculation before we update the histogram with sideband data
-            cov_mat_unisim = hist_generator.calculate_unisim_uncertainties(central_value_hist=hist, extra_query=extra_query)
+                extended_cov_mat_contrib = covariance(concat_observations, extended_central_value)
+                # sanity check: the upper left block of the extended covariance should be the same
+                # as cov_mat, and the lower right block should be the same as sb_cov_mat
+                assert np.all(np.equal(extended_cov_mat_contrib[: hist.n_bins, : hist.n_bins], cov_mat))
+                assert np.all(np.equal(extended_cov_mat_contrib[hist.n_bins :, hist.n_bins :], sb_cov_mat))
+                extended_cov_mat += extended_cov_mat_contrib
+                sideband_cov_mat += sb_cov_mat
 
-            # If we are using a sideband, we can now calculate the correction to the central value 
+            cov_mat_unisim = hist_generator.calculate_unisim_uncertainties(
+                central_value_hist=hist, extra_query=extra_query
+            )
+            hist.add_covariance(cov_mat_unisim)
+            # If we are using a sideband, we can now calculate the correction to the central value
             # and the covariance matrix from the sideband data.
             if use_sideband:
-                constrained_cov_mat, constrained_mu = constrained_covariance(
+                sideband_data_hist = self.sideband_generator.get_data_hist()
+                sideband_ext_hist = self.sideband_generator.get_data_hist(type="ext")
+                sideband_total_prediction = sideband_mc_cv_hist + sideband_ext_hist
+                mu_offset, cov_corr = sideband_constraint_correction(
+                    # The data that was actually observed
                     sideband_measurement=sideband_data_hist.nominal_values,
-                    obs_central_value=hist.nominal_values,
-                    sideband_central_value=sideband_central_value_hist.nominal_values,
-                    concat_cov=extended_cov_mat,
+                    # to calculate the constraint, we want to compare the data to the total prediction
+                    # in the sideband, which is the MC prediction plus the beam-off EXT data
+                    sideband_central_value=sideband_total_prediction.nominal_values,
+                    concat_covariance=extended_cov_mat,
+                    # We also want to use the covariance matrix of the total prediction for the
+                    # sideband, which includes the covariance of the MC and EXT data
+                    sideband_covariance=sideband_total_prediction.cov_matrix,
                 )
-                # make a new histogram where we replace the nominal values and the covariance matrix
-                # with the constrained ones.
-                hist_dict = hist.to_dict()
-                hist_dict["bin_counts"] = constrained_mu
-                hist_dict["covariance_matrix"] = constrained_cov_mat
-                hist = Histogram.from_dict(hist_dict)
+                # finally we add the corrections to the histogram
+                hist += mu_offset
+                hist.add_covariance(cov_corr)
 
-            # finally, add the unisim covariance matrix to the multisim covariance matrix.
-            hist.add_covariance(cov_mat_unisim)
         hist *= scale_factor
         return hist
 
@@ -998,7 +1008,7 @@ class TestHistogram(unittest.TestCase):
         # check covariance matrix
         np.testing.assert_array_almost_equal(cov_matrix, expected_div_hist.cov_matrix, decimal=4)
 
-    # Test conversion to and from dict 
+    # Test conversion to and from dict
     def test_dict_conversion(self):
         bin_edges = np.array([0, 1, 2, 3])
         bin_counts = np.array([1, 2, 3])
@@ -1007,6 +1017,7 @@ class TestHistogram(unittest.TestCase):
         hist_dict = hist.to_dict()
         hist_from_dict = Histogram.from_dict(hist_dict)
         self.assertEqual(hist, hist_from_dict)
+
 
 if __name__ == "__main__":
     unittest.main(argv=[""], verbosity=2, exit=False)

plotting/statistics.py
@@ -4,7 +4,6 @@ import unittest
 import numpy as np
 
 
-
 def covariance(observations, central_value=None):
     """Calculate the covariance matrix of the given observations.
 
@@ -48,32 +47,99 @@ def covariance(observations, central_value=None):
         return cov / observations.shape[0]
 
 
-def constrained_covariance(
-    sideband_measurement, obs_central_value, sideband_central_value, observations=None, sideband_observations=None, concat_cov=None
+def get_cnp_covariance(expectation, observation):
+    """Get the combined Neyman-Pearson covaraince matrix.
+
+    This matrix may be used to calculate a chi-square between the expectation and observation
+    in a way that gives the optimal compromise between the Neyman and Pearson constructions.
+
+
+    Parameters
+    ----------
+    expectation : array_like
+        Array of expectation values.
+    observation : array_like
+        Array of observations.
+
+    Returns
+    -------
+    cnp_covariance : array_like
+        Combined Neyman-Pearson covariance matrix.
+
+    Notes
+    -----
+    The combined Neyman-Pearson covariance matrix is given by
+
+    .. math::
+        C_{ij} = 3 / \\left( \\frac{2}{\\mu_i} + \\frac{1}{n_i} \\right) \\delta_{ij}
+
+    where :math:`\\mu_i` and :math:`n_i` are the expectation and observation, respectively.
+    For a mathematical derivation see arXiv:1903.07185.
+    In this form, however, we can get division by zero errors when the observation is zero.
+    We rearrange the equation instead to
+
+    .. math::
+        C_{ij} = 3 \\mu_i n_i \\delta_{ij} / \\left( \\mu_i + 2 n_i \\right)
+    """
+
+    expectation = np.asarray(expectation)
+    observation = np.asarray(observation)
+    if expectation.shape != observation.shape:
+        raise ValueError("Expectation and observation must have the same shape.")
+    if expectation.ndim != 1:
+        raise ValueError("Expectation and observation must be 1D arrays.")
+    if np.any(expectation <= 0):
+        raise ValueError("Expectation must be positive.")
+    if np.any(observation < 0):
+        raise ValueError("Observation must be non-negative.")
+    cnp_covariance = np.diag(3 * expectation * observation / (expectation + 2 * observation))
+    return cnp_covariance
+
+
+def sideband_constraint_correction(
+    sideband_measurement,
+    sideband_central_value,
+    obs_central_value=None,
+    observations=None,
+    sideband_observations=None,
+    concat_covariance=None,
+    sideband_covariance=None,
 ):
-    """Calculate the covariance on the observations given the sideband measurement.
+    """Calculate the corrections to the covariance and nominal values given the sideband measurement.
 
     This follows the Block-matrix prescription that can be found in
     https://en.wikipedia.org/wiki/Covariance_matrix#Block_matrices
 
     Parameters
     ----------
-    sideband_measurement : array_like
-        Measurement of the sideband.
-    obs_central_value : array_like
-        Central value of the observations.
     sideband_central_value : array_like
         Central value of the sideband observations.
+    sideband_measurement : array_like
+        Measurement of the sideband.
+    obs_central_value : array_like or None
+        Central value of the observations. Not needed if concat_covariance is given.
     observations : array_like or None
-        Array of observations. If None, the concat_cov must be given.
+        Array of observations. If None, the concat_covariance must be given.
     sideband_observations : array_like or None
-        Array of sideband observations. If None, the concat_cov must be given.
-    concat_cov : array_like, optional
+        Array of sideband observations. If None, the concat_covariance must be given.
+    concat_covariance : array_like, optional
         Covariance matrix of the concatenated observations. If not given, it will be calculated
         from the observations and the central value.
+    sideband_covariance : array_like, optional
+        Covariance matrix of the sideband. If given, this replaces the lower right corner of the
+        concatenated covariance. This is expected to only contain the MC uncertainty of the
+        sideband, as the Neyman-Pearson covariance that takes care of the data uncertainty is
+        added in this function.
+
+    Returns
+    -------
+    mu_offset : array_like
+        Offset of the nominal values.
+    covariance_correction : array_like
+        Correction to the covariance matrix.
     """
 
-    if concat_cov is None:
+    if concat_covariance is None:
         assert observations is not None
         assert sideband_observations is not None
         # First, we concatenate the observations and the sideband observations.
@@ -83,21 +149,30 @@ def constrained_covariance(
         concat_central_value = np.concatenate((obs_central_value, sideband_central_value))
         # calculate the covariance matrix of the concatenated observations
         cov = covariance(concat_observations, concat_central_value)
+        n = len(obs_central_value)
     else:
-        cov = concat_cov
-    n = len(obs_central_value)
+        cov = concat_covariance
+        n = cov.shape[0] - len(sideband_central_value)
     # extract the blocks of the covariance matrix
-    Kxx = cov[: n, : n]
-    Kxy = cov[: n, n :]
-    Kyx = cov[n :, : n]
-    Kyy = cov[n :, n :]
+    Kxx = cov[:n, :n]
+    Kxy = cov[:n, n:]
+    Kyx = cov[n:, :n]
+    Kyy = cov[n:, n:]
+    if sideband_covariance is not None:
+        # check that the size is the same
+        assert sideband_covariance.shape[0] == Kyy.shape[0]
+        assert sideband_covariance.shape[1] == Kyy.shape[1]
+        Kyy = sideband_covariance
+    # We also add the CNP covariance to the sideband
+    Kyy += get_cnp_covariance(sideband_central_value, sideband_measurement)
 
     # now we can calculate the conditional mean
-    mu = obs_central_value + Kxy @ np.linalg.inv(Kyy) @ (sideband_measurement - sideband_central_value)
+    mu_offset = Kxy @ np.linalg.inv(Kyy) @ (sideband_measurement - sideband_central_value)
     # and the conditional covariance
-    cond_cov = Kxx - Kxy @ np.linalg.inv(Kyy) @ Kyx
+    covariance_correction = -Kxy @ np.linalg.inv(Kyy) @ Kyx
 
-    return cond_cov, mu
+    return mu_offset, covariance_correction
+
 
 def error_propagation_division(x1, x2, C1, C2):
     """