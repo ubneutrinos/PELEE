224ca69427c21a84e4560f6c7c7c2e9c375f9274
Alex Trettin <alexander.trettin@physik.hu-berlin.de>
2023-09-18 14:43:19 +0100

    Get multiband two-hypo test working

--- plotting/analysis.py
+++ plotting/analysis.py
@@ -13,7 +13,7 @@ from plotting.histogram import Binning, HistogramGenerator, RunHistGenerator, Hi
 from plotting.parameters import ParameterSet
 from plotting.run_plotter import RunHistPlotter
 from plotting import signal_generators
-from plotting.statistics import sideband_constraint_correction
+from plotting.statistics import sideband_constraint_correction, chi_square
 
 
 class MultibandAnalysis(object):
@@ -45,6 +45,7 @@ class MultibandAnalysis(object):
         self._signal_configurations = signal_configurations
         self.signal_names = [config.get("name", config["selection"]) for config in signal_configurations]
         rundata, weights, data_pot = load_runs(**configuration["signal_data"])
+        self.data_pot = data_pot
         self._signal_generators = [
             self.run_hist_generator_from_config(rundata, weights, data_pot, config) for config in signal_configurations
         ]
@@ -93,7 +94,9 @@ class MultibandAnalysis(object):
         )
         return run_generator
 
-    def generate_multiband_histogram(self, include_multisim_errors=False, use_sideband=False, strict_covar_checking=False):
+    def generate_multiband_histogram(
+        self, include_multisim_errors=False, use_sideband=False, strict_covar_checking=False, scale_to_pot=None
+    ):
         """Generate a combined histogram from all signal bands."""
 
         total_nbins = sum([g.binning.n_bins for g in self._signal_generators])
@@ -108,10 +111,15 @@ class MultibandAnalysis(object):
             # If we don't include the multisim errors, we are done
             combined_nominal_values = np.concatenate([h.nominal_values for h in stat_only_signal_hists])
             combined_uncertainty = np.concatenate([h.std_devs for h in stat_only_signal_hists])
-            return Histogram(global_binning, combined_nominal_values, uncertainties=combined_uncertainty)
+            output_hist = Histogram(global_binning, combined_nominal_values, uncertainties=combined_uncertainty)
+            if scale_to_pot is not None:
+                output_hist *= scale_to_pot / self.data_pot
+            return output_hist
         # combine the histograms
         combined_nominal_values = np.concatenate([h.nominal_values for h in stat_only_signal_hists])
-        combined_covariance = self._get_total_multiband_covariance(with_stat_only=True, with_unisim=True, include_sideband=False)
+        combined_covariance = self._get_total_multiband_covariance(
+            with_stat_only=True, with_unisim=True, include_sideband=False
+        )
         # sanity check: the diagonal blocks of the combined covariance should be the same
         # as if we had calculated the histograms with multisim separately
         pos = 0
@@ -126,18 +134,23 @@ class MultibandAnalysis(object):
                 combined_nominal_values[pos : pos + n_bins], combined_nominal_values[pos : pos + n_bins]
             )
             # When we are using non-standard histogram generators such as the signal-over-background generator,
-            # it is possible that the block matrices are not equivalent. In that case, we only check for 
+            # it is possible that the block matrices are not equivalent. In that case, we only check for
             # loose agreement. However, when the standard HistogramGenerator class is used, this really should
             # work correctly.
             np.testing.assert_allclose(reference_covar, covar, atol=1e-6 if strict_covar_checking else 1e-1)
             pos += n_bins
         if not use_sideband:
-            return Histogram(global_binning, combined_nominal_values, covariance_matrix=combined_covariance)
+            output_hist = Histogram(global_binning, combined_nominal_values, covariance_matrix=combined_covariance)
+            if scale_to_pot is not None:
+                output_hist *= scale_to_pot / self.data_pot
+            return output_hist
         sideband_prediction = self._sideband_generator.get_total_prediction()
         sideband_observation = self._sideband_generator.data_hist_generator.generate()
         # To make the covariance matrix, we need to first generate the full multiband covariance
         # and then apply the sideband constraint.
-        multiband_covariance_multisim = self._get_total_multiband_covariance(with_stat_only=False, with_unisim=False, include_sideband=True)
+        multiband_covariance_multisim = self._get_total_multiband_covariance(
+            with_stat_only=False, with_unisim=False, include_sideband=True
+        )
         # Now we can calculate the sideband correction. This is done only for multisim errors.
         delta_mu, delta_covar = sideband_constraint_correction(
             sideband_measurement=sideband_observation.nominal_values,
@@ -150,8 +163,11 @@ class MultibandAnalysis(object):
         combined_nominal_values += delta_mu
         combined_covariance += delta_covar
 
-        return Histogram(global_binning, combined_nominal_values, covariance_matrix=combined_covariance)
-    
+        output_hist = Histogram(global_binning, combined_nominal_values, covariance_matrix=combined_covariance)
+        if scale_to_pot is not None:
+            output_hist *= scale_to_pot / self.data_pot
+        return output_hist
+
     def generate_multiband_data_histogram(self):
         """Generate a combined histogram from all signal bands."""
 
@@ -159,10 +175,10 @@ class MultibandAnalysis(object):
         # make a Binning of the global bin number
         bin_edges = np.arange(total_nbins + 1)
         global_binning = Binning("None", bin_edges, label="Global bin number")
-        data_hists = [
-            g.data_hist_generator.generate()
-            for g in self._signal_generators
-        ]
+        data_hists = [g.data_hist_generator.generate() for g in self._signal_generators]
+        # If any of the data histograms is None, we return None
+        if any([h is None for h in data_hists]):
+            return None
         # combine the histograms
         combined_nominal_values = np.concatenate([h.nominal_values for h in data_hists])
         combined_uncertainty = np.concatenate([h.std_devs for h in data_hists])
@@ -254,7 +270,7 @@ class MultibandAnalysis(object):
             ax.text(-1, pos - hist_gen.binning.n_bins / 2, label, ha="right", va="center", fontsize=12)
         return fig, ax
 
-    def two_hypothesis_test(self, h0_params, h1_params, sensitivity_only=False):
+    def two_hypothesis_test(self, h0_params, h1_params, sensitivity_only=False, n_trials=1000, scale_to_pot=None):
         """Perform a two hypothesis test between two parameter sets.
 
         For each hypothesis, pseudo-experiments are generated by sampling
@@ -268,33 +284,112 @@ class MultibandAnalysis(object):
         null hypothesis).
 
         The resulting dictionary contains the following keys:
-        - "chi2": The chi-square of the observed data.
-        - "pval": The p-value of the observed chi-square under H0.
+        - "chi2_h0": The chi-square of the observed data with respect to H0.
+        - "pval_h0": The p-value of the observed chi-square under H0.
+        - "chi2_h1": The chi-square of the observed data with respect to H1.
         - "pval_h1": The p-value of the observed chi-square under H1.
-        - "chi2_median_h1": The median chi-square of the pseudo-experiments
+        - "ts_median_h1": The median test statistic of the pseudo-experiments
           sampled under H1.
-        - "median_pval": The p-value of the median chi-square under H0.
-        - "samples_h0": The chi-square of the pseudo-experiments sampled
+        - "median_pval": The p-value of the median test statistic under H0.
+        - "samples_h0": The test statistic of the pseudo-experiments sampled
           under H0.
-        - "samples_h1": The chi-square of the pseudo-experiments sampled
+        - "samples_h1": The test statistic of the pseudo-experiments sampled
           under H1.
 
-        Args:
-            h0_params (ParameterSet): The parameters of the first hypothesis.
-            h1_params (ParameterSet): The parameters of the second hypothesis.
+        Parameters
+        ----------
+        h0_params : ParameterSet
+            The parameters of the null hypothesis.
+        h1_params : ParameterSet
+            The parameters of the alternative hypothesis.
+        sensitivity_only : bool
+            If True, only the p-value of the median test statistic under H0
+            is calculated. This is useful for calculating the sensitivity of
+            an analysis.
+        n_trials : int
+            The number of pseudo-experiments to generate.
+        scale_to_pot : float
+            If given, scale the histograms to this POT value before generating
+            the pseudo-experiments. Only a sensitivity can be produced in this
+            case.
 
-        Returns:
-            dict : Dictionary containing the results of the hypothesis test.
+        Returns
+        -------
+        dict
+            A dictionary containing the results of the test.
         """
 
+        if scale_to_pot is not None:
+            assert sensitivity_only, "Can only scale to POT when calculating sensitivity"
         assert set(h0_params.names) == set(h1_params.names), "Parameter sets must have the same parameters"
-        assert self.parameters.names == set(
-            h0_params.names
-        ), "Parameter sets must have the same parameters as the analysis"
-        # set parameters to H0, be sure to set the *values* and not the objects in order
-        # not to break the linking between parameters of the signal bands
-        for name in h0_params.names:
-            self.parameters[name].value = h0_params[name].value
+        self.set_parameters(h0_params, check_matching=True)
+        # generate the multiband histogram
+        h0_hist = self.generate_multiband_histogram(include_multisim_errors=True, use_sideband=True, scale_to_pot=scale_to_pot)
+
+        self.set_parameters(h1_params, check_matching=True)
+        h1_hist = self.generate_multiband_histogram(include_multisim_errors=True, use_sideband=True, scale_to_pot=scale_to_pot)
+
+        test_stat_h0 = []
+        test_stat_h1 = []
+
+        def test_statistic(observation):
+            chi2_h0 = chi_square(observation.nominal_values, h0_hist.nominal_values, h0_hist.cov_matrix)
+            chi2_h1 = chi_square(observation.nominal_values, h1_hist.nominal_values, h1_hist.cov_matrix)
+            return chi2_h0 - chi2_h1
+
+        for i in range(n_trials):
+            pseudo_data_h0 = h0_hist.fluctuate(seed=4 * i).fluctuate_poisson(seed=4 * i + 1)
+            pseudo_data_h1 = h1_hist.fluctuate(seed=4 * i + 2).fluctuate_poisson(seed=4 * i + 3)
+
+            test_stat_h0.append(test_statistic(pseudo_data_h0))
+            test_stat_h1.append(test_statistic(pseudo_data_h1))
+
+        test_stat_h0 = np.array(test_stat_h0)
+        test_stat_h1 = np.array(test_stat_h1)
+
+        results = dict()
+        results["ts_median_h1"] = np.median(test_stat_h1)
+        results["median_pval"] = np.sum(test_stat_h0 > results["ts_median_h1"]) / n_trials
+        results["samples_h0"] = test_stat_h0
+        results["samples_h1"] = test_stat_h1
+
+        data_hist = self.generate_multiband_data_histogram()
+        if data_hist is None or sensitivity_only:
+            # If the analysis has been run in blind mode, then the data histogram is None
+            # and we are done. Fill None for the remaining results keys.
+            results["chi2_h0"] = None
+            results["pval_h0"] = None
+            results["chi2_h1"] = None
+            results["pval_h1"] = None
+
+            return results
+        # calculate the p-value of the observed data under H0
+        real_data_ts = test_statistic(data_hist)
+        results["chi2_h0"] = chi_square(data_hist.nominal_values, h0_hist.nominal_values, h0_hist.cov_matrix)
+        results["pval_h0"] = np.sum(test_stat_h0 > real_data_ts) / n_trials
+        # calculate the p-value of the observed data under H1
+        results["chi2_h1"] = chi_square(data_hist.nominal_values, h1_hist.nominal_values, h1_hist.cov_matrix)
+        results["pval_h1"] = np.sum(test_stat_h1 > real_data_ts) / n_trials
+
+        return results
+
+    def set_parameters(self, parameter_set, check_matching=True):
+        """Set the parameters of the analysis.
+
+        Parameters
+        ----------
+        parameter_set : ParameterSet
+        check_matching : bool
+            If True, check that the parameter set contains all the parameters
+            of the analysis. If False, only the parameters that are present
+            in the parameter set are set.
+        """
+        if check_matching:
+            assert set(parameter_set.names) == set(
+                self.parameters.names
+            ), "Parameter sets must have the same parameters"
+        for name in parameter_set.names:
+            self.parameters[name].value = parameter_set[name].value
 
     @classmethod
     def from_toml(cls, file_path):
