d8a0f45efd4455685c8a84a65b0d2fc185a3f10a
Alex Trettin <alexander.trettin@physik.hu-berlin.de>
2023-11-21 13:40:55 +0000

    Add custom JSON encoder and decoder for Microfit objects

--- microfit/fileio.py
+++ microfit/fileio.py
@@ -2,29 +2,36 @@
 
 import numpy as np
 import json
+from .histogram import Binning, Histogram
 
-
-# Custom JSON Encoder that can handle numpy arrays
-class NumpyEncoder(json.JSONEncoder):
+# Custom JSON Encoder that can handle numpy arrays and other objects
+class MicrofitEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
             # Add a flag to know it was a numpy array
-            return {"_is_numpy_array": True, "data": obj.tolist()}
-        return super(NumpyEncoder, self).default(obj)
+            return {"_obj_type": "ndarray", "data": obj.tolist()}
+        elif type(obj) in [Binning, Histogram]:
+            return {"_obj_type": type(obj).__name__, "data": obj.to_dict()}
+        return super(MicrofitEncoder, self).default(obj)
 
 
 # Custom JSON Decoder to handle numpy arrays
-def numpy_decoder(dct):
-    if "_is_numpy_array" in dct:
-        return np.array(dct["data"])
+def microfit_decoder(dct):
+    if "_obj_type" in dct:
+        if dct["_obj_type"] == "ndarray":
+            return np.array(dct["data"])
+        elif dct["_obj_type"] == "Binning":
+            return Binning.from_dict(dct["data"])
+        elif dct["_obj_type"] == "Histogram":
+            return Histogram.from_dict(dct["data"])
     return dct
 
 
 def from_json(filename):
     with open(filename, "r") as f:
-        return json.load(f, object_hook=numpy_decoder)
+        return json.load(f, object_hook=microfit_decoder)
 
 
 def to_json(filename, data):
     with open(filename, "w") as f:
-        json.dump(data, f, indent=4, cls=NumpyEncoder)
+        json.dump(data, f, indent=4, cls=MicrofitEncoder)
