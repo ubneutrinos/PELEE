aaeea99fb36cae9f487256c7d67cf745c82d3d85
Alex Trettin <alexander.trettin@physik.hu-berlin.de>
2024-01-23 07:01:55 -0600

    Forward kwargs to colorbar

--- microfit/histogram/histogram.py
+++ microfit/histogram/histogram.py
@@ -84,7 +84,7 @@ class Histogram:
             raise ValueError("Either uncertainties or covariance_matrix must be provided.")
 
     def draw_covariance_matrix(
-        self, ax=None, as_correlation=True, as_fractional=False, **plot_kwargs
+        self, ax=None, as_correlation=True, as_fractional=False, colorbar_kwargs=None, **plot_kwargs
     ):
         """Draw the covariance matrix on a matplotlib axis.
 
@@ -133,7 +133,7 @@ class Histogram:
                 X, Y, self.covariance_matrix, cmap=colormap, vmin=vmin, vmax=vmax, **plot_kwargs
             )
             label = "Covariance"
-        cbar = plt.colorbar(pc, ax=ax)
+        cbar = plt.colorbar(pc, ax=ax, **(colorbar_kwargs or {}))
         cbar.set_label(label)
         ax.set_xlabel(self.binning.variable_tex)  # type: ignore
         ax.set_ylabel(self.binning.variable_tex)  # type: ignore
@@ -889,7 +889,7 @@ class MultiChannelHistogram(Histogram):
         unrolled_hist = self.get_unrolled_histogram()
         ax = unrolled_hist.draw(ax, as_errorbars, show_errors, **plot_kwargs)
         channel_n_bins = np.cumsum([0] + [len(b) for b in self.binning])
-        channel_labels = [b.selection_tex or b.label for b in self.binning]
+        channel_labels = [b.selection_tex_short or b.label for b in self.binning]
         for n_bins in channel_n_bins[1:-1]:
             ax.axvline(n_bins, color="k", linestyle="--")
 
@@ -910,7 +910,7 @@ class MultiChannelHistogram(Histogram):
     def draw_covariance_matrix(self, ax=None, as_correlation=True, **plot_kwargs):
         ax = self.get_unrolled_histogram().draw_covariance_matrix(ax, as_correlation, **plot_kwargs)
         channel_n_bins = np.cumsum([0] + [len(b) for b in self.binning])
-        channel_labels = [b.selection_tex or b.label for b in self.binning]
+        channel_labels = [b.selection_tex_short or b.label for b in self.binning]
         for n_bins in channel_n_bins[1:-1]:
             ax.axvline(n_bins, color="k", linestyle="--")
             ax.axhline(n_bins, color="k", linestyle="--")
