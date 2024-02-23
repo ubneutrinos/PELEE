cb7537f6cbe789f81549f4a45401a310f28607f0
Alex Trettin <alexander.trettin@physik.hu-berlin.de>
2024-02-23 09:05:30 -0600

    Auto-format make_detsys and some adjustments

--- make_detsys.py
+++ make_detsys.py
@@ -1,70 +1,42 @@
-import argparse
-import sys
+from typing import List, Optional, Union
 
 import data_loading as dl
-from microfit.histogram import Binning, HistogramGenerator, RunHistGenerator
-from microfit.fileio import to_json,from_json
+from microfit.histogram import Binning, HistogramGenerator, MultiChannelBinning
+from microfit.fileio import to_json, from_json
 import logging
 import os
-import hashlib
 import localSettings as ls
-
-def _get_mc_filter_query(filter_queries):
-    """Given a list of filter queries, combine and then negate them.
-    
-    The result is a query that will get the same sample as was used to make
-    the 'mc' histogram in the detector variations.
-    """
-    query = " & ".join(filter_queries)
-    query = "not (" + query + ")"
-    return query
 
 
 def make_variation_histograms(
-    run,
-    dataset,
-    variation,
-    selection_query,
-    binning,
-    use_kde_smoothing=False,
-    **dl_kwargs 
+    run: List[str], dataset: str, variation: str, binning: Union[Binning, MultiChannelBinning], use_kde_smoothing: bool = False, extra_selection_query: Optional[str] = None, **dl_kwargs
 ):
-
     """Load the data and Make the json file to store the detector variation predictions"""
 
-    rundata, _, data_pot = dl.load_runs_detvar(
-        run,
-        dataset,
-        variation,
-        **dl_kwargs
-    )
-
-    # We want the variations to be the events per POT
-    #for sample in rundata.keys():
-    #    rundata[sample]["weights"] /= data_pot
-    #    rundata[sample]["weights_no_tune"] /= data_pot
+    rundata, _, data_pot = dl.load_runs_detvar(run, dataset, variation, **dl_kwargs)
 
     hist_dict = {}
     if use_kde_smoothing:
         # Skip covariance calculation for KDE smoothing since we only need the CV
-        options={"bound_transformation": "both", "calculate_covariance": False}
+        options = {"bound_transformation": "both", "calculate_covariance": False}
     else:
-        options={}
+        options = {}
     for dataset in rundata:
-        df = rundata[dataset].query(selection_query, engine="python")
-        generator = HistogramGenerator(df, binning)
-        hist_dict[dataset] = generator.generate(use_kde_smoothing=use_kde_smoothing, options=options)
-    #filter_queries["mc"] = _get_mc_filter_query(list(filter_queries.values()))
+        generator = HistogramGenerator(rundata[dataset], binning)
+        hist_dict[dataset] = generator.generate(
+            use_kde_smoothing=use_kde_smoothing, options=options, extra_query=extra_selection_query
+        )
 
     return hist_dict
 
-def make_detvar_plots(detvar_data, output_dir, plotname):
 
+def make_detvar_plots(detvar_data, output_dir, plotname):
     """Make plots of the histograms contained in the detvar data"""
 
     import matplotlib.pyplot as plt
 
     for truth_filter in detvar_data["variation_hist_data"]:
+        print("Making plots for", truth_filter)
         fig, ax = plt.subplots()
         hist_dict = detvar_data["variation_hist_data"][truth_filter]
         hist_dict["cv"].draw(ax=ax, label="CV", color="k", show_errors=False, lw=3)
@@ -72,171 +44,100 @@ def make_detvar_plots(detvar_data, output_dir, plotname):
             if name == "cv":
                 continue
             hist.draw(ax=ax, label=name, show_errors=False)
-        #ax.set_ylabel("Events / POT")
-        ax.set_ylabel("Events")
-        ax.set_xlabel(detvar_data["binning"].variable)
-        ax.set_ylim(bottom=0)
-        ax.legend(ncol=2)
-        ax.set_title(f"Dataset: {truth_filter}, Selection: {detvar_data['selection_key']}")
+        # ax.set_ylabel("Events / POT")
+        # ax.set_ylabel("Events")
+        # ax.set_xlabel(detvar_data["binning"].variable)
+        # ax.set_ylim(bottom=0)
+        # ax.legend(ncol=2)
+        ax.set_title(f"Dataset: {truth_filter}")
 
         fig.savefig(output_dir + truth_filter + "_" + plotname)
-         
-        ''' 
-        fig.savefig(
-            os.path.join(
-                output_dir, f"detvar_{truth_filter}_{detvar_data['selection_key']}.pdf",
-            ),
-            bbox_inches="tight",
-        )
-        '''
 
-# New function to enable integration of detvar generation into
-# notebooks insetad of as a separate function
+def _sanitize_selection_query(query: str) -> str:
+    """Remove any characters that are not legal inside a file name from the selection query."""
 
-def make_variations(RUN,dataset,selection,preselection,binning,use_kde_smoothing=False,make_plots=False,plot_output_dir="",enable_detvar_cache=False,**dl_kwargs):
+    return query.replace(" ", "_").replace(">", "gt").replace("<", "lt").replace("=", "eq").replace("!", "not").replace("&", "and").replace("|", "or")
 
+def _negate_query(query: str) -> str:
+    """Negate the selection query."""
+
+    return "!(" + query + ")"
+
+def make_variations(
+    run_numbers: List[str],
+    data: str,
+    binning: Union[Binning, MultiChannelBinning],
+    selection: Optional[str] = None,
+    preselection: Optional[str] = None,
+    use_kde_smoothing: bool = False,
+    make_plots: bool = False,
+    plot_output_dir: str = ".",
+    enable_detvar_cache: bool = False,
+    detvar_cache_dir: Optional[str] = None,
+    extra_selection_query: Optional[str] = None,
+    **dl_kwargs,
+):
     runcombo_str = ""
-    for i_r in range(0,len(RUN)): runcombo_str = runcombo_str + RUN[i_r]
-    output_file = "run_" + runcombo_str + "_" + preselection + "_" + selection + "_" + binning.variable
-    detvar_file = ls.detvar_cache_path + "/" + output_file + ".json"
+    for i_r in range(0, len(run_numbers)):
+        runcombo_str = runcombo_str + run_numbers[i_r]
+    channels_str = "_".join(binning.channels)
+    output_file = (
+        "run_" + runcombo_str + "_" + channels_str + "_" + data
+    )
+    if extra_selection_query is not None:
+        output_file += "_" + _sanitize_selection_query(extra_selection_query)
+    detvar_file = ls.detvar_cache_path or detvar_cache_dir
+    assert detvar_file is not None, "detvar_cache_dir must be provided if detvar_cache_path is not set"
+    detvar_file += "/" + output_file + ".json"
 
     if enable_detvar_cache and os.path.isfile(detvar_file):
-        print("Loading devar histograms from file:",detvar_file) 
-        return from_json(detvar_file) 
+        print("Loading devar histograms from file:", detvar_file)
+        detvar_data = from_json(detvar_file)
+        if make_plots:
+            make_detvar_plots(detvar_data, plot_output_dir, output_file + ".pdf")
+        return detvar_data
 
-    selection_query = RunHistGenerator.get_selection_query(
-        selection=selection, preselection=preselection
-    )
-    logging.debug(f"Selection query: {selection_query}")
+    if isinstance(binning, MultiChannelBinning):
+        assert selection is None and preselection is None, "Cannot pass selection and preselection with MultiChannelBinning"
+    elif binning.selection_query is None:
+        assert selection is not None, "Selection must be provided if binning.selection_query is None"
+        binning.set_selection(selection=selection, preselection=preselection)
+    logging.debug(f"Binning: {binning}")
     variation_hist_data = {}
-    filter_queries = {}
     variations = dl.detector_variations
 
     for variation in variations:
         logging.info(f"Making histogram for variation {variation}")
-        (
-            variation_hist_data[variation]
-        ) = make_variation_histograms(
-            RUN,
-            dataset,
+        variation_hist_data[variation] = make_variation_histograms(
+            run_numbers,
+            data,
             variation,
-            selection_query,
             binning,
             use_kde_smoothing=use_kde_smoothing,
-            **dl_kwargs
+            extra_selection_query=extra_selection_query,
+            **dl_kwargs,
         )
 
     # Switch ordering of keys in the variation_hist_data dict. Instead of
     # variation_hist_data[variation][dataset], we want to have
     # variation_hist_data[dataset][variation]
-
     variation_hist_data = {
-        dataset: {
-            variation: variation_hist_data[variation][dataset]
-            for variation in variations
-        }
+        dataset: {variation: variation_hist_data[variation][dataset] for variation in variations}
         for dataset in variation_hist_data[variations[0]]
     }
-    # Also, we can assume that the filter_queries are the same for all
-    # variations, so we can just take the first one
-    #filter_queries = filter_queries[variations[0]]
-
-
-    binning.selection_key = selection
-    binning.preselection_key = preselection
 
     detvar_data = {
-        "run": RUN,
-        "selection_key": selection,
-        "preselection_key": preselection,
-        "selection_query": selection_query,
+        "run": run_numbers,
         "binning": binning,
         "variation_hist_data": variation_hist_data,
-        #"filter_queries": filter_queries,
-        "mc_sets": list(variation_hist_data.keys())
+        "mc_sets": list(variation_hist_data.keys()),
+        "extra_selection_query": extra_selection_query,
     }
 
     if enable_detvar_cache:
-        to_json(detvar_file,detvar_data)
+        to_json(detvar_file, detvar_data)
 
     if make_plots:
-        make_detvar_plots(detvar_data,plot_output_dir,output_file+".pdf")
+        make_detvar_plots(detvar_data, plot_output_dir, output_file + ".pdf")
 
     return detvar_data
-
-'''
-def main(args):
-    RUN = args.run
-    selection = args.selection
-    preselection = args.preselection
-    truth_filtered_sets = args.truth_filtered_sets
-    numu=args.numu
-    use_kde_smoothing=args.kde_smoothing
-    binning_def = list(args.binning_def.split(","))
-
-    binning_def[1] = int(binning_def[1])
-    binning_def[2] = (float(binning_def[2]), float(binning_def[3]))
-    del binning_def[3]
-    binning = Binning.from_config(*binning_def)
-
-    binning.selection_key = args.selection
-    binning.preselection_key = args.preselection
-
-    logging.debug(repr(binning))
-
-    make_variations(RUN,selection,preselection,truth_filtered_sets,numu,use_kde_smoothing,binning,args.output_file,args.make_plots,args.plot_output_dir)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Make variation histograms")
-    parser.add_argument("--run", nargs='+', help="Run number (as space separated list of strings)", required=True)
-    parser.add_argument(
-        "--selection", type=str, help="Selection criteria", required=True
-    )
-    parser.add_argument(
-        "--preselection", type=str, help="Preselection criteria", required=True
-    )
-    parser.add_argument("--output-file", type=str, help="Output file", required=True)
-    parser.add_argument(
-        "--binning-def",
-        type=str,
-        help="Binning definition. Required format is: ('variable_name', n_bins, min, max)",
-        required=True,
-    )
-    parser.add_argument(
-        "-v", "--verbosity", action="count", default=0, help="Increase output verbosity"
-    )
-    parser.add_argument(
-        "--truth-filtered-sets",
-        type=str,
-        nargs="*",
-        default=["nue"],
-        help="List of truth-filtered sets to use",
-    )
-    parser.add_argument(
-        "--make-plots", action="store_true", help="Make plots of the histograms",
-    )
-    parser.add_argument(
-        "--plot-output-dir", type=str, default=".", help="Output directory for plots",
-    )
-    parser.add_argument(
-        "--numu", action="store_true", help="Use numu selection instead of nue",
-    )
-    parser.add_argument(
-        "--kde-smoothing", action="store_true", help="Use KDE smoothing",
-    )
-    args = parser.parse_args()
-
-    log_levels = [logging.WARNING, logging.INFO, logging.DEBUG]
-    log_level = log_levels[min(args.verbosity, 2)]
-    logging.basicConfig(level=log_level)
-
-    output_dir = os.path.dirname(args.output_file)
-    if not os.path.exists(output_dir):
-        print(f"Error: Output directory '{output_dir}' does not exist.")
-        sys.exit(1)
-
-    main(args)
-'''
-
-
