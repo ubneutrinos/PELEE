Fix detvar with extra query * Fixes issue where passing an extra_query to a SignalOverBackgroundGenerator gave unexpected covariance when including detvars * Adds unit tests for detvar calculation * Adds unit test that asserts that the fixed SignalOverBackgroundGenerator gives the expected result

microfit/tests/test_run_hist_generator.py
@@ -1,12 +1,16 @@
 from typing import Union
 import unittest
+from unittest.mock import patch
 import pandas as pd
 import numpy as np
-from microfit.histogram import Binning
+from microfit.histogram import Binning, MultiChannelBinning, MultiChannelHistogram
 from microfit.histogram import RunHistGenerator
 from microfit.histogram import MultiChannelBinning
 from microfit.parameters import ParameterSet, Parameter
 from microfit.signal_generators import SignalOverBackgroundGenerator
+
+from microfit.tests.test_detsys import MockLoadRunsDetvar
+from microfit.detsys import make_variations
 
 
 class TestRunHistGenerator(unittest.TestCase):
@@ -209,6 +213,135 @@ class TestRunHistGenerator(unittest.TestCase):
             mc_hist_generator_cls=SignalOverBackgroundGenerator,
             parameters=signal_parameters,
             mc_hist_generator_kwargs=sob_kwargs,
+        )
+
+    @patch("microfit.detsys.dl.load_runs_detvar")
+    def test_with_detvar(self, mock_load_runs_detvar):
+        # Create a mock dataframe
+        mock_df = self.make_dataframe()
+
+        # Create a mock object that behaves like load_runs_detvar
+        mock_load_runs_detvar.side_effect = MockLoadRunsDetvar(mock_df)
+
+        # Create a MultiChannelBinning
+        multi_binning = self.make_test_binning(
+            multichannel=True, with_query=True, second_query="non_matching"
+        )
+
+        # Generate mock detvar_data
+        detvar_data = make_variations(
+            ["1", "2", "3"],
+            "dataset",
+            multi_binning,
+            make_plots=False,
+            variations=["cv", "up", "down"],
+        )
+
+        # Create mock run data
+        mock_rundata = {
+            "mc": mock_df,
+            "data": self.make_dataframe(n_samples=100),
+            "ext": self.make_dataframe(n_samples=10),
+        }
+
+        # Instantiate RunHistGenerator with detvar_data
+        run_hist_generator = RunHistGenerator(
+            mock_rundata, multi_binning, data_pot=1.0, detvar_data=detvar_data
+        )
+
+        # Test get_mc_hist without add_precomputed_detvars
+        mc_hist = run_hist_generator.get_mc_hist()
+        self.assertIsInstance(mc_hist, MultiChannelHistogram)
+
+        # Test get_mc_hist with add_precomputed_detvars
+        mc_hist_with_detvars = run_hist_generator.get_mc_hist(
+            add_precomputed_detsys=True, include_detsys_variations=["cv", "up", "down"]
+        )
+        self.assertIsInstance(mc_hist_with_detvars, MultiChannelHistogram)
+
+    @patch("microfit.detsys.dl.load_runs_detvar")
+    def test_detvar_filter_equivalence(self, mock_load_runs_detvar):
+        # This function tests the RunHistGenerator when it is used with the
+        # SignalOverBackgroundGenerator and detvar_data combined. The thing
+        # we are testing here is that generating a histogram with signal strength
+        # set to zero gives the same result as passing the extra_query
+        # "not is_signal" to the RunHistGenerator. This should be asserted
+        # with and without multisim errors.
+
+        # Create a mock dataframe
+        mock_df = self.make_dataframe()
+
+        # Create a mock object that behaves like load_runs_detvar
+        mock_load_runs_detvar.side_effect = MockLoadRunsDetvar(mock_df)
+
+        # Create a MultiChannelBinning
+        multi_binning = self.make_test_binning(
+            multichannel=True, with_query=True, second_query="non_matching"
+        )
+
+        # Generate mock detvar_data
+        detvar_data = make_variations(
+            ["1", "2", "3"],
+            "dataset",
+            multi_binning,
+            make_plots=False,
+            variations=["cv", "up", "down"],
+        )
+
+        # Create mock run data, this time including a signal channel
+        mock_rundata = {
+            "mc": mock_df,
+            "data": self.make_dataframe(n_samples=100),
+            "ext": self.make_dataframe(n_samples=10),
+            "signal": self.make_dataframe(
+                n_samples=100, weights_scale=0.1, signal_flag=True, with_multisim=True
+            ),
+        }
+
+        signal_parameters = ParameterSet(
+            [
+                Parameter("signal_strength", 1.0, bounds=(0, 10)),  # type: ignore
+            ]
+        )
+        sob_kwargs = {
+            "signal_query": "is_signal",
+            "background_query": "not is_signal",
+        }
+
+        run_hist_generator = RunHistGenerator(
+            mock_rundata,
+            multi_binning,
+            data_pot=1.0,
+            parameters=signal_parameters,
+            detvar_data=detvar_data,
+            mc_hist_generator_cls=SignalOverBackgroundGenerator,
+            **sob_kwargs,  # type: ignore
+        )
+
+        mc_hist_no_detvar = run_hist_generator.get_mc_hist()
+        mc_hist_with_detvar = run_hist_generator.get_mc_hist(
+            add_precomputed_detsys=True, include_detsys_variations=["cv", "up", "down"]
+        )
+
+        self.assertIsInstance(mc_hist_no_detvar, MultiChannelHistogram)
+        self.assertIsInstance(mc_hist_with_detvar, MultiChannelHistogram)
+
+        run_hist_generator.parameters["signal_strength"].value = 0.0
+        mc_hist_0str = run_hist_generator.get_mc_hist(
+            add_precomputed_detsys=True, include_detsys_variations=["cv", "up", "down"]
+        )
+        run_hist_generator.parameters["signal_strength"].value = 1.0
+        mc_hist_extra_query = run_hist_generator.get_mc_hist(
+            extra_query="not is_signal",
+            add_precomputed_detsys=True,
+            include_detsys_variations=["cv", "up", "down"],
+        )
+
+        np.testing.assert_array_almost_equal(
+            mc_hist_0str.bin_counts, mc_hist_extra_query.bin_counts
+        )
+        np.testing.assert_array_almost_equal(
+            mc_hist_0str.covariance_matrix, mc_hist_extra_query.covariance_matrix
         )
 
 