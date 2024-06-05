722a4457e1a0ad5b6353edd254b7b5137d4a2bbb
Alex Trettin <alexander.trettin@physik.hu-berlin.de>
2024-06-05 06:28:55 -0500

    Fix capitalization, formatting

--- microfit/run_plotter.py
+++ microfit/run_plotter.py
@@ -140,7 +140,7 @@ class RunHistPlotter:
         total_pred_hist.tex_string = "Total (MC)"
         if ext_hist is not None:
             total_pred_hist += ext_hist
-            total_pred_hist.tex_string = "Total Predicted"
+            total_pred_hist.tex_string = "Total predicted"
         if use_sideband:
             total_pred_hist.tex_string += ",\nconstrained"
         # This should not be the method to blind the analysis! The only purpose of this
@@ -612,7 +612,7 @@ class RunHistPlotter:
         y = np.array([repeated_nom_values(hist) for hist in hists])
         labels = [hist.tex_string for hist in hists]
         if show_counts:
-            labels = [f"{label}: {hist.sum():.1f}" for label, hist in zip(labels, hists)]
+            labels = [f"{label}: {hist.sum():.0f}" for label, hist in zip(labels, hists)]
         colors = None
         colors = [hist.color for hist in hists]
         # Hatches may be None
